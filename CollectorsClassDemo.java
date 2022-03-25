a76c53fe5696ea9242117e897ba2e0041a4aa3eb
mohdanas2411 <anas06132@gmail.com>
2022-03-25 11:38:30 +0530

    push

--- CollectorsClassDemo.java
+++ CollectorsClassDemo.java
@@ -68,13 +68,36 @@ public class CollectorsClassDemo {
         System.out.println("\n\nusing Collectors.PartitionsBy \n");
         List<Integer> elements = Arrays.asList(10, 2, 3, 55, 123, 99, 98);
         Map<Boolean, List<Integer>> isEven = elements.stream().collect(Collectors.partitioningBy(val -> val % 2 == 0));
-        System.out.println("Even Value List  : "+isEven.get(true));
-        System.out.println("Odd Value List  : "+isEven.get(false));
+        System.out.println("Even Value List  : " + isEven.get(true));
+        System.out.println("Odd Value List  : " + isEven.get(false));
 
 
         //overloaded PartitionsBy take 2 argument as predicate and Collectors.<any method of collectors class>
         Map<Boolean, Long> count = elements.stream().collect(Collectors.partitioningBy(val -> val % 2 == 0, Collectors.counting()));
-        System.out.println("Number of even elements in the list : "+count.get(true));
-        System.out.println("Number of odd elements in the list : "+count.get(false));
+        System.out.println("Number of even elements in the list : " + count.get(true));
+        System.out.println("Number of odd elements in the list : " + count.get(false));
+
+
+        //groupingBy take 1 argument as Function and the value that returned by this function become the key of our map.
+        System.out.println("\n\nusing Collectors.groupingBy \n");
+        Map<String, List<Employee>> group1 = emplist.stream().collect(Collectors.groupingBy(Employee::getName));
+        System.out.println("group1  groupingBy with 1 parameter  : " + group1);
+
+        //groupingBy take 2 argument as Function and the value that returned by this function become the key of our map.
+        Map<String, Set<Employee>> group2 = emplist.stream().collect(Collectors.groupingBy(Employee::getName, Collectors.toSet()));
+        System.out.println("group2  groupingBy with 2 parameter  : " + group2);
+
+        //groupingBy take 3 argument as Function and the value that returned by this function become the key of our map , supplier only Map type , collectors
+        LinkedHashMap<String, Set<Employee>> group3 = emplist.stream().collect(Collectors.groupingBy(Employee::getName, LinkedHashMap::new, Collectors.toSet()));
+        System.out.println("group3  groupingBy with 3 parameter  : " + group3);
+
+        LinkedHashMap<String, Map<String, Double>> group4 = emplist.stream().collect(Collectors.groupingBy(Employee::getName, LinkedHashMap::new, Collectors.toMap(Employee::getName, Employee::getSalary)));
+        System.out.println("group3  groupingBy with 3 parameter Map inside a Map  : " + group4);
+
+
+        // methods of Colloctors.reducing Class
+        System.out.println("\n\nusing Colloctors.reducing Class\n");
+        Double result = emplist.stream().collect(Collectors.reducing(0.0, employee -> employee.getSalary() * 10, (exix, newVal) -> exix + newVal));
+        System.out.println("reduce the employees salary and return the sum multiplied by 10  :  " + result);
     }
 }
